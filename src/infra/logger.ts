23e0dbd9e24868dfa398bde7c9f0034043fd282c
Caio Abe <caio.abe@estilo7.com.br>
2021-01-02 19:04:31 -0300

    Renames logger interface

--- src/infra/logger.ts
+++ src/infra/logger.ts
@@ -2,5 +2,5 @@ import { Logger } from './types';
 
 export const logger: Logger = {
   // eslint-disable-next-line no-console
-  publish: (message: string): void => console.error(message),
+  log: (message: string): void => console.error(message),
 };
--- src/infra/types.ts
+++ src/infra/types.ts
@@ -1,3 +1,3 @@
 export interface Logger {
-  publish: (message: string) => void;
+  log: (message: string) => void;
 }
